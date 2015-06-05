8777078cf199aa94f17ad57e7a229f097a08bd4d
yumashev <a.yumashev@gmail.com>
2015-06-06 02:56:59 +0500

    change post meta and chg version

--- inc/template-parts/content-single.php
+++ inc/template-parts/content-single.php
@@ -9,6 +9,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+        <?php the_post_thumbnail( 'large', array( 'class' => 'thumbnail img-responsive' )); ?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
--- inc/template-parts/content.php
+++ inc/template-parts/content.php
@@ -9,16 +9,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+        <a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'large', array( 'class' => 'thumbnail img-responsive' )); ?></a>
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wpbss' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+            if ( is_single() or empty(get_the_excerpt())) {
+                /* translators: %s: Name of current post */
+                the_content( sprintf(
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wpbss' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ) );
+            } else {
+	               the_excerpt();
+            }
 		?>
 
 		<?php
