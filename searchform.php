b2187216ffc974218c8c4327a60ba29bfda8bd11
yumashev <a.yumashev@gmail.com>
2015-06-06 21:30:16 +0500

    upd lcl and style and ver

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
 ?>
 <form role="search" method="get" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="form-group">
-        <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Text for search...', 'placeholder', 'wpbss' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Найти:">
+        <input type="search" class="search-field form-control" placeholder="<?php _e( 'Text for search...', 'wpbss' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Найти:">
     </div>
-    <input type="submit" class="btn btn-default search-submit" value="<?php echo esc_attr_x( 'Find', 'submit button', 'wpbss' ); ?>">
+    <input type="submit" class="btn btn-default search-submit" value="<?php _e( 'Find', 'wpbss' ); ?>">
 </form>
