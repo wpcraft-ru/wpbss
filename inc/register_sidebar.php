a1035b9cef22a2cde3cf345c600b0ccbd49d4731
yumashev <a.yumashev@gmail.com>
2015-06-06 19:38:30 +0500

    update translate

--- inc/register_sidebar.php
+++ inc/register_sidebar.php
@@ -6,7 +6,7 @@ Register 3 sidebars for header.php
 register_sidebar(array(
 	'name' => __('Header 1'),
 	'id' => 'header-widgets-1',
-	'description' => __('Виджеты в шапке 1'),
+	'description' => __('Header Widgets 1'),
 	'before_widget' => '',
 	'after_widget' => '',
 	'before_title' => '<h3><a href="#">',
@@ -15,7 +15,7 @@ register_sidebar(array(
 register_sidebar(array(
 	'name' => __('Header 2'),
 	'id' => 'header-widgets-2',
-	'description' => __('Виджеты в шапке 2'),
+	'description' => __('Header Widgets 2'),
 	'before_widget' => '',
 	'after_widget' => '',
 	'before_title' => '<h3><a href="#">',
@@ -24,7 +24,7 @@ register_sidebar(array(
 register_sidebar(array(
 	'name' => __('Header 3'),
 	'id' => 'header-widgets-3',
-	'description' => __('Виджеты в шапке 3'),
+	'description' => __('Header Widgets 3'),
 	'before_widget' => '',
 	'after_widget' => '',
 	'before_title' => '<h3><a href="#">',
@@ -38,7 +38,7 @@ Register 3 sidebars for header.php
 register_sidebar(array(
 	'name' => __('Footer 1'),
 	'id' => 'footer-widgets-1',
-	'description' => __('Виджеты в подвале 1'),
+	'description' => __('Footer Widgets 1'),
 	'before_widget' => '',
 	'after_widget' => '',
 	'before_title' => '<h3><a href="#">',
@@ -47,7 +47,7 @@ register_sidebar(array(
 register_sidebar(array(
 	'name' => __('Footer 2'),
 	'id' => 'footer-widgets-2',
-	'description' => __('Виджеты в подвале 2'),
+	'description' => __('Footer Widgets 2'),
 	'before_widget' => '',
 	'after_widget' => '',
 	'before_title' => '<h3><a href="#">',
