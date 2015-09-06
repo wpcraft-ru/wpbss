d1f1a08f3f4c3a72944ed4e6d58e55a651c7cea9
yumashev <a.yumashev@gmail.com>
2015-09-06 09:45:08 +0500

    fix woo commerce search form and chg version

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -1,6 +1,8 @@
-<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
+<form role="search" method="get" class="woocommerce-product-search form-inline" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
 	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'woocommerce' ); ?></label>
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
-	<input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
+	<div class="form-group">
+		<input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
+	</div>
+	<input type="submit" class="btn btn-default" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
 	<input type="hidden" name="post_type" value="product" />
 </form>
