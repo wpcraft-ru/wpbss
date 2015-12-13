19b6301761481d2a59f2414ae091c8fe6dffd67d
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-14 00:25:15 +0500

    Update page-no-title.php

--- page-no-title.php
+++ page-no-title.php
@@ -7,36 +7,38 @@ Template Name: Page. No title
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area col-xs-12">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                        <?php
-                            wp_link_pages( array(
-                                'before' => '<div class="page-links">' . __( 'Pages:', 'wpbss' ),
-                                'after'  => '</div>',
-                            ) );
-                        ?>
-                    </div><!-- .entry-content -->
-                    
-                </article><!-- #post-## -->
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+<div class="container">
+	<div class="row">
+		<div id="primary" class="content-area col-xs-12">
+			<main id="main" class="site-main" role="main">
+	
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+	                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	                    <div class="entry-content">
+	                        <?php the_content(); ?>
+	                        <?php
+	                            wp_link_pages( array(
+	                                'before' => '<div class="page-links">' . __( 'Pages:', 'wpbss' ),
+	                                'after'  => '</div>',
+	                            ) );
+	                        ?>
+	                    </div><!-- .entry-content -->
+	                    
+	                </article><!-- #post-## -->
+	
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+	
+				<?php endwhile; // end of the loop. ?>
+	
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .row -->
+</div><!-- .container -->
 <?php get_footer(); ?>
