e2181ed7b35a6492e13c1b62fa5a30e0dcb5a062
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-14 01:03:18 +0500

    Create functions.php

---
+++ wpbss-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// Faster than @import and stable http://getbootstrap.com/getting-started/#support-ie8-respondjs
+add_action( 'wp_enqueue_scripts', 'wpbss_import_base_style' );
+function wpbss_import_base_style() {
+    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+}
