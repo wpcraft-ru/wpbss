2992f74bf5c984a88de144f87c414115950ad586
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-14 00:26:25 +0500

    Update page.php

--- page.php
+++ page.php
@@ -11,23 +11,26 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area col-xs-12">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'inc/template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div class="container">
+	<div class="row">
+		<div id="primary" class="content-area col-xs-12">
+			<main id="main" class="site-main" role="main">
+	
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<?php get_template_part( 'inc/template-parts/content', 'page' ); ?>
+	
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+	
+				<?php endwhile; // end of the loop. ?>
+	
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .row -->
+</div><!-- .container -->
 <?php get_footer(); ?>
