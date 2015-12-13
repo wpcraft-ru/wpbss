579ec441c0a3fba732a5aed69b53cc9c0b82ad84
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-14 00:25:50 +0500

    Update page-sidebar.php

--- page-sidebar.php
+++ page-sidebar.php
@@ -11,24 +11,27 @@ Template Name: Page + Sidebar
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area col-md-8 col-xs-12 pull-right">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'inc/template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-<?php get_sidebar(); ?>
+<div class="container">
+	<div class="row">
+		<div id="primary" class="content-area col-md-8 col-xs-12 pull-right">
+			<main id="main" class="site-main" role="main">
+	
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<?php get_template_part( 'inc/template-parts/content', 'page' ); ?>
+	
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+	
+				<?php endwhile; // end of the loop. ?>
+	
+			</main><!-- #main -->
+		</div><!-- #primary -->
+		<?php get_sidebar(); ?>
+	</div><!-- .row -->
+</div><!-- .container -->
 <?php get_footer(); ?>
