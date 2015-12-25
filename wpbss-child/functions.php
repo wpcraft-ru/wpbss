2c5754d570cf6c341cce40828d7bf4141acc63bf
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-25 15:36:34 +0500

    Update functions.php

--- wpbss-child/functions.php
+++ wpbss-child/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Faster than @import and stable http://getbootstrap.com/getting-started/#support-ie8-respondjs
-add_action( 'wp_enqueue_scripts', 'wpbss_import_base_style' );
+add_action( 'wp_enqueue_scripts', 'wpbss_import_base_style', 22 );
 function wpbss_import_base_style() {
     wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 }
