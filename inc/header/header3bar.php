48a1f3f9bfc84e425f1820ecf3409df2011e57ad
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-12-28 16:04:28 +0500

    Update header3bar.php

--- inc/header/header3bar.php
+++ inc/header/header3bar.php
@@ -3,33 +3,39 @@
 /*###############################
 Register 3 sidebars for header.php
 */
-register_sidebar(array(
-	'name' => __('Header 1'),
-	'id' => 'header-widgets-1',
-	'description' => __('Header Widgets 1'),
-	'before_widget' => '',
-	'after_widget' => '',
-	'before_title' => '<h3>',
-	'after_title' => '</h3>',
-));
-register_sidebar(array(
-	'name' => __('Header 2'),
-	'id' => 'header-widgets-2',
-	'description' => __('Header Widgets 2'),
-	'before_widget' => '',
-	'after_widget' => '',
-	'before_title' => '<h3>',
-	'after_title' => '</h3>',
-));
-register_sidebar(array(
-	'name' => __('Header 3'),
-	'id' => 'header-widgets-3',
-	'description' => __('Header Widgets 3'),
-	'before_widget' => '',
-	'after_widget' => '',
-	'before_title' => '<h3>',
-	'after_title' => '</h3>',
-));
+
+function wpbss_widgets_h3_init() {
+
+	register_sidebar(array(
+		'name' => __('Header 1'),
+		'id' => 'header-widgets-1',
+		'description' => __('Header Widgets 1'),
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
+	register_sidebar(array(
+		'name' => __('Header 2'),
+		'id' => 'header-widgets-2',
+		'description' => __('Header Widgets 2'),
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
+	register_sidebar(array(
+		'name' => __('Header 3'),
+		'id' => 'header-widgets-3',
+		'description' => __('Header Widgets 3'),
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
+}
+add_action( 'widgets_init', 'wpbss_widgets_h3_init', 20 );
+
 
 function header_add_tmpl_3widgets(){
 
